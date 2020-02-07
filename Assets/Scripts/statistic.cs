a272c93ba384aaf6d15c61397b749fcef15a73b3
Muhammad Usman <muhammad.usman@ug.bilkent.edu.tr>
2020-02-07 15:51:42 +0300

    Update statistic.cs

--- Assets/Scripts/statistic.cs
+++ Assets/Scripts/statistic.cs
@@ -5,12 +5,11 @@ using System;
 using System.Text.RegularExpressions;
 using UnityEngine.SceneManagement;
 public class statistic : MonoBehaviour {
-    public Text Username;
+	public Text Username;
 	public Text Mail;
-    public Text Level1;
-    public Text Level2;
-    public Text status;
-	//public string uname = Login.user;
+    	public Text Level1;
+    	public Text Level2;
+    	public Text status;
 	public static int level1acc;
 	public static int level2acc;
 
@@ -20,22 +19,17 @@ public class statistic : MonoBehaviour {
 	private int userstatistic1;
 	private int userstatistic2;
 
-    //string statURL = "https://lechief.azurewebsites.net/statistic.php";
+
 	string statURL = "localhost/lechief/statistic.php";
-    // Use this for initialization
+	
+    	// Use this for initialization
 	public void gobackButton(){
 		SceneManager.LoadScene("Main");
 	}
-    void Start () {
+	
+    	void Start () {
 		Username.text = Login.user;
 		StartCoroutine(stat(Login.user));
-		/*Debug.Log ("Splitter 1 is : " + splitter [1]);
-		/int.TryParse (splitter [1], out userstatistic);
-		int.TryParse (splitter [3], out userstatistic1);
-		result = userstatistic + userstatistic1;
-		Debug.Log (result);
-		accuracy [0] = userstatistic;
-		accuracy [1] = userstatistic1;*/
 	}
 	
 	// Update is called once per frame
@@ -53,31 +47,24 @@ public class statistic : MonoBehaviour {
     }
 
     IEnumerator stat(string uname)
-    {/*
-       WWWForm form = new WWWForm();
-		form.AddField("usernamePost", uname);
-        //form.AddField("passwordPost", pass);
+    {
 
-		WWW site = new WWW(statURL,form);
-        yield return site;
-        Debug.Log(site.text);
-*/
-
-		WWWForm form = new WWWForm();
-		form.AddField("usernamePost", uname);
+	WWWForm form = new WWWForm();
+	form.AddField("usernamePost", uname);
 		
-		WWW site = new WWW(statURL,form);
-		yield return site;
-		Debug.Log (site.text);
-		splitter = site.text.Split(char.Parse(","));
-		Level1.text = "Level"+splitter [0] + " Your accuracy is : " + splitter [1];
-		Level2.text = "Level"+splitter [2] + " Your Accuracy is : " + splitter [3];
-		Mail.text = splitter [4];
-		result = int.Parse (splitter [1]) + int.Parse (splitter [3]);
-		level1acc = int.Parse (splitter [1]);
-		Debug.Log ("Accuracy 1: " + level1acc);
-		level2acc = int.Parse (splitter [3]);
-		Debug.Log ("Accuracy 2: " + level2acc);
-		Debug.Log ("result is : " + result);
+	WWW site = new WWW(statURL,form);
+	yield return site;
+	Debug.Log (site.text);
+	splitter = site.text.Split(char.Parse(","));
+	Level1.text = "Level"+splitter [0] + " Your accuracy is : " + splitter [1];
+	Level2.text = "Level"+splitter [2] + " Your Accuracy is : " + splitter [3];
+	Mail.text = splitter [4];
+	result = int.Parse (splitter [1]) + int.Parse (splitter [3]);
+	level1acc = int.Parse (splitter [1]);
+	Debug.Log ("Accuracy 1: " + level1acc);
+	level2acc = int.Parse (splitter [3]);
+	Debug.Log ("Accuracy 2: " + level2acc);
+	Debug.Log ("result is : " + result);
+	    
     }
 }
